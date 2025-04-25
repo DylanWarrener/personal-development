dcae03505ec6e78cb3f63029a02dd4ffcdea24c8
DylanWarrener <dqyqlqaqn@gmail.com>
2025-04-25 14:58:42 +0100

    Adding middleware

---
+++ Programming/Javascript/Server/Frameworks/Express/2. Middleware/index.js
@@ -0,0 +1,17 @@
+const http = require('http')
+
+const express = require('express')
+
+const app = express()
+
+app.use((req, res, next) => {
+    console.log('First middleware')
+    next()
+})
+
+app.use((req, res, next) => {
+    console.log('Second middleware')
+    res.send('<h1>Hello from Express!</h1>')
+})
+
+app.listen(3000)
